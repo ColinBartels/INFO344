12f171db6a12331afc2afb18148bba648072773b
Colin Bartels <colin.0011@hotmail.com>
2016-06-13 17:24:44 -0700

    Update jsonp.php

--- HW 4/jsonp.php
+++ HW 4/jsonp.php
@@ -3,7 +3,7 @@ if(isset($_GET['value'])) {
    $value = $_GET['value'];
 
    try {
-       $conn = new PDO('mysql:host=nba.cdxfydz9vy1z.us-west-2.rds.amazonaws.com;port:3306;dbname=NBA', 'colin', 'feather!');
+       $conn = new PDO('mysql:host=nba.cdxfydz9vy1z.us-west-2.rds.amazonaws.com;port:3306;dbname=NBA', 'colin', *****);
        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
        $stmt = $conn->prepare("SELECT * FROM NBA.Stats WHERE Name LIKE :value");
        $stmt->bindValue(':value', $value);
